1f3c72aad4cfd07c7bdc3ae4e8432da4f437e1e7
JA <69608172+ClassJay@users.noreply.github.com>
2024-07-30 07:48:50 -0500

    Added associations to the models

--- models/index.js
+++ models/index.js
@@ -6,15 +6,23 @@ const ProductTag = require('./ProductTag');
 const router = require('express').Router()
 
 // Products belongsTo Category
+Product.belongsTo(Category)
 router.use('/Category', Category)
 
 // Categories have many Products
+Category.hasMany(Product)
 router.use('/Category/Product', Product)
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, { 
+  through: ProductTag
+})
 router.use('/Product/ProductTag', ProductTag)
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag
+})
 router.use('/ProductTag/Tag', Tag)
 
 module.exports = {
