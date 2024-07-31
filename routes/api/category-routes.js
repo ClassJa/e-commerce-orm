de9d7f4d27d2af4fd5091f81cc6968cebaa60d3a
JA <69608172+ClassJay@users.noreply.github.com>
2024-07-30 21:50:20 -0500

    Fixed the logic for the routes in the category-routes file

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -21,49 +21,45 @@ router.get('/:id', async (req, res) => {
     // find one category by its `id` value
   // be sure to include its associated Products
   try {
-    await Category.findOne({
+    const foundCategory = await Category.findOne({
       where: {
         id: req.params.id
     },
 }
-)}
+)
+res.json(foundCategory)
+}
   catch(e) {
     res.json("Error: ", e)
-    res.status(400)
   }
-  res.json("Category found!")
-  res.status(200)
 });
 
 router.post('/', async (req, res) => {
     // create a new category
   try {
-    await Category.create(
+    const newCategory = await Category.create(
       {
-        // id: req.params.id,
-        category_name: req.params.body.category_name
+        // id: req.body.id,
+        category_name: req.body.category_name
       }
-    ).then((newCategory) => {
-      res.json("New Category created", newCategory)
-      res.status(200)
-    })
+    )
+    console.log(newCategory)
+    res.json(newCategory)
   }
-  // Is this try catch block the right syntax?
   catch(e) {
-    res.json("Error occured when trying to create a new category", e)
-    res.status(400)
+    res.status(400).json(e)
   }
 });
 
 router.put('/:id', async (req, res) => {
     // update a category by its `id` value
-  await Category.update(
+ try { 
+  const updatedCategory = await Category.update(
     {
-      id: req.params.body,
-      category_name: req.params.body
+      // id: req.body.id,
+      category_name: req.body.category_name
     },
     // list out the different properties that should get updated
-
   {
     where: {
       id: req.params.id
@@ -71,10 +67,11 @@ router.put('/:id', async (req, res) => {
   }
   // check if this the correct set-up
 )
-.catch((e) => {
-  res.json("Error occurred", e)
-})
-res.json("Update completed for id: ", req.params.id)
+res.json(updatedCategory)
+}
+catch(e){
+  res.status(400).json(e)
+}
 });
 
 router.delete('/:id', async (req, res) => {
@@ -86,12 +83,8 @@ router.delete('/:id', async (req, res) => {
     res.json("Category removed!")
   })
   .catch((e) => {
-    res.json("Error occurred when trying to delete this category", e)
-    res.status(400)
+    res.status(400).json(e)
   })
-  // res._destroy(req.params.id)
-  // check if this the correct set-up
-  // delete a category by its `id` value
 });
 
 module.exports = router;
